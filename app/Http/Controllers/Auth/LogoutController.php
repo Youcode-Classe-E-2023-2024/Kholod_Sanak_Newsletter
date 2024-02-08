7980958936f9cb7d9ff71bcf3cf82e9da48a084d
kholoud001 <kholoud.sanak@gmail.com>
2024-02-08 10:20:10 +0100

    logout controller

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -4,17 +4,17 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class LogoutController extends Controller
 {
     public function destroy(Request $request)
     {
-        Auth::logout();
 
+        Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/login');
     }
-
 }
