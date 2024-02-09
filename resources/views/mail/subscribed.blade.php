217702040ccbce85575537127714d505fcf18b3c
kholoud001 <kholoud.sanak@gmail.com>
2024-02-09 16:02:02 +0100

    subcribed message

--- resources/views/mail/subscribed.blade.php
+++ resources/views/mail/subscribed.blade.php
@@ -1,1 +1,26 @@
-<h1> User has subscribed !!!! </h1>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@^2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <title>Subscribed</title>
+</head>
+<body>
+<div class="flex items-center justify-center min-h-screen p-5 bg-blue-100 min-w-screen">
+    <div class="max-w-xl p-8 text-center text-gray-800 bg-white shadow-xl lg:max-w-3xl rounded-3xl lg:p-12">
+        <h3 class="text-2xl">Subscription to our newsletter!</h3>
+        <div class="flex justify-center">
+            <img src="https://img.freepik.com/free-vector/thank-you-lettering_1262-6963.jpg?size=626&ext=jpg" alt="Success Icon" style="width: 100px; height: auto;" class="text-green-400">
+        </div>
+
+        <p>We're happy you're here. </p>
+        <div class="mt-4">
+            <p class="mt-4 text-sm">Welcome to our newsletter community! You're now subscribed to receive the latest updates, promotions, and news directly to your inbox. Stay tuned for exciting content!
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
