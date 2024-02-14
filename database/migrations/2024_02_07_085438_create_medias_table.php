a3fdd55b1e83155f987fbe3ff2f451407a1456ed
kholoud001 <kholoud.sanak@gmail.com>
2024-02-14 17:27:50 +0100

    permission seeder update

--- database/migrations/2024_02_07_085438_create_medias_table.php
+++ database/migrations/2024_02_07_085438_create_medias_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->enum('vid', ['img'])->default('img');
             $table->unsignedBigInteger('user_id');
-
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
