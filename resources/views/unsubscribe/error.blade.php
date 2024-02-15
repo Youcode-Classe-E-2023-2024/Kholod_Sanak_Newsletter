37d91b10662d6251e91e9bf666738e169f5ccceb
kholoud001 <kholoud.sanak@gmail.com>
2024-02-15 11:36:19 +0100

    usunb error blade

--- resources/views/unsubscribe/error.blade.php
+++ resources/views/unsubscribe/error.blade.php
@@ -1,17 +1,36 @@
-<?php
-
-
-?>
-    <!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@^2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+    <title>Unsubscription Error</title>
 </head>
 <body>
-<h1> error in unsubscription </h1>
+<div class=" h-screen">
+    <div class="bg-white p-6  md:mx-auto">
+        <svg viewBox="0 0 24 24" class="text-red-600 w-16 h-16 mx-auto my-6">
+            <path fill="currentColor"
+                  d="M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z">
+            </path>
+        </svg>
+
+
+
+        <div class="text-center">
+            <h3 class="md:text-2xl text-base text-gray-900 font-semibold text-center">Error in Unsubscription </h3>
+             <p class="text-gray-600 my-2">Please recheck your email</p>
+             <p> Have a great day!  </p>
+            <div class="py-10 text-center">
+                <a href="{{route('home')}}" class="px-12 bg-indigo-600 hover:bg-indigo-500 text-white font-semibold py-3">
+                    GO BACK
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
