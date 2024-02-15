0ed519ed51dd4440c63f334badb9a9931a70aa58
kholoud001 <kholoud.sanak@gmail.com>
2024-02-15 11:35:56 +0100

    success email page

--- resources/views/password/success.blade.php
+++ resources/views/password/success.blade.php
@@ -10,7 +10,7 @@
     <title>Email success</title>
 </head>
 <body>
-<div class="bg-gray-100 h-screen">
+<div class=" h-screen">
     <div class="bg-white p-6  md:mx-auto">
         <svg viewBox="0 0 24 24" class="text-green-600 w-16 h-16 mx-auto my-6">
             <path fill="currentColor"
